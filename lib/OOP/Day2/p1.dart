7258ce809ba56a2b91db756c8c0a0e3320cdceea
Ashiqur Rahman Ashiq <ashiqurashiq54@gmail.com>
2025-08-17 04:51:45 +0600

    add bank account list and total amount calculation

--- lib/OOP/Day2/p1.dart
+++ lib/OOP/Day2/p1.dart
@@ -1,4 +1,7 @@
 void main() {
+
+  double totalAmount = 0;
+
   BankAccount ac1 = BankAccount();
   BankAccount ac2 = BankAccount();
 
@@ -9,12 +12,20 @@ void main() {
   ac2.accountName = 'Rakib';
   ac2.accountNumber = 02;
   ac2.balance = 1000;
-
-
   ac2.transferAmount(500, ac1);
 
   print(ac1.balance);
   print(ac2.balance);
+
+  List<BankAccount> bankAccountList = [];
+  bankAccountList.add(ac1);
+  bankAccountList.add(ac2);
+
+  for(BankAccount bankAccount in bankAccountList) {
+    totalAmount += bankAccount.balance;
+  }
+
+  print("------TotalAmount------>>$totalAmount");
 }
 
 
